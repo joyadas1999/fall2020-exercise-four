0a85a8f3e0a4d69eb4c66e09854beba3fbf98e1e
joyadas1999 <47679860+joyadas1999@users.noreply.github.com>
2020-11-24 05:09:34 -0500

    added express and environmental files

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 
 const port = process.env.POST || 4000;
 
-app.use("/", indeRoute);
+const indexRoute = require("./routes/index.js");
+
+app.use("/", indexRoute);
 
 app.listen(port, () =>
   console.log(`Exercise Four is running at localhost: ${port}`)
