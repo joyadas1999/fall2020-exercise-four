601049b7cd426b4c510004bca7ea05606e84f1ba
joyadas1999 <47679860+joyadas1999@users.noreply.github.com>
2020-12-02 04:56:20 -0500

    add post queries individually

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,29 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("A Blogpost"));
+const firebase = require("firebase");
+const db = firebase.firestore();
+const blogposts = db.collection("blogposts");
+
+router.get("/", (req, res) => res.send("No Id provided"));
+
+router.get("/:id", (req, res) => {
+  const queryId = req.params.id;
+  blogposts
+    .doc(queryId)
+    .get()
+    .then(function(doc) {
+      if (doc.exists) {
+        const data = doc.data();
+
+        return res.send(data);
+      } else {
+        return res.send("No document exists");
+      }
+    })
+    .catch(function(error) {
+      return res.send(error);
+    });
+});
 
 module.exports = router;
