6bc9648a1720a2a0254cb83ee4dba4b1c9e8a0c4
joyadas1999 <47679860+joyadas1999@users.noreply.github.com>
2020-12-08 03:04:26 -0500

    Add create post

--- routes/createBlogpost.js
+++ routes/createBlogpost.js
@@ -1,7 +1,33 @@
 const express = require("express");
 
 const router = express.Router();
+const firebase = require("firebase");
+const db = firebase.firestore();
+const blogposts = db.collection("blogposts");
 
-router.get("/", (req, res) => res.send("Create a Blogpost"));
+const form = `
+<form action="/create/submit"> 
+<input type="text" name="title" placeholder= "Title of Post" />
+<input type="text" name="title" placeholder= "Text of Post" />
+<input type="text" name="title" placeholder= "Author" />
+<button type = "submit> Submit Post</button>
+</form>
+`;
+
+router.get("/", (req, res) => res.send(form));
+router.get("/submit", (req, res) => {
+  const queryParams = req.query;
+  const idFromTitle = queryParams.title.replace(/\s+/g, "-").toLowerCase();
+  blogposts
+    .doc(idFromTitle)
+    .set(queryParams)
+    .then(function(doc) {
+      res.send("Successful Submission");
+    })
+    .catch(function(error) {
+      console.log("error", error);
+      res.send("Failed Submission");
+    });
+});
 
 module.exports = router;
